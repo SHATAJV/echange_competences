54f3286918eed768e0cf0b52d0d488f0858417da
shahrzad Tajvidi <shahrzad.tajvidi@fms-ea.com>
2024-11-12 11:30:16 +0100

    add connextion

--- myapp/allo_aide/urls.py
+++ myapp/allo_aide/urls.py
@@ -1,1 +1,12 @@
+# Allô_aide/urls.py
 from django.urls import path
+from . import views
+
+app_name = 'allo_aide'
+
+urlpatterns = [
+    path('', views.home, name='home'),  # Page d'accueil pour les visiteurs
+    path('user/', views.home_user, name='home_user'),  # Page d'accueil pour utilisateurs connectés
+    path('login/', views.login_view, name='login'),  # Page de connexion
+    path('logout/', views.logout_view, name='logout'),  # Déconnexion
+]
--- myapp/allo_aide/views.py
+++ myapp/allo_aide/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.shortcuts import render, redirect
 from .models import Skill, TimeSlot
 
 
@@ -6,3 +9,27 @@ def home(request):
     skills = Skill.objects.all()
     timeslots = TimeSlot.objects.filter(is_available=True)  # Affiche uniquement les créneaux disponibles
     return render(request, 'allo_aide/home.html', {'skills': skills, 'timeslots': timeslots})
+
+@login_required
+def home_user(request):
+    return render(request, 'allo_aide/home_user.html')
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('allo_aide:home_user')  # Redirige vers la page des utilisateurs connectés
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'allo_aide/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('allo_aide:home')
